ca3f9ef63454f1069603e048109258130a0e479d
John MacLeod <johnmacleod1@Johns-MacBook-Pro-4.local>
2019-03-13 11:26:01 +1100

    First

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+try:
+    from pip._internal import main as pip_main
+except:
+    from pip import main as pip_main
+
+pip_main(['install', '--upgrade', 'git+https://github.com/ibm-watson-iot/functions.git@'])
+
+setup(
+  name='asdemo',
+  version='1.0.5',
+  packages=find_packages(),
+  dependency_links=['git+https:github.com/ibm-watson-iot/functions.git@']
+  )
+
+
